2a04a6516cd1f053a60d0f75076b741d880ba3d0
Andrey S <andrey.sitaev@gmail.com>
2020-08-07 21:46:17 +0300

    Added symbol - frequency to corpus/compare page

--- vman/apps/vgui/models/corpus_brief.py
+++ vman/apps/vgui/models/corpus_brief.py
@@ -1,3 +1,5 @@
+import codecs
+import os
 from typing import List, Tuple, Dict
 
 from apps.vnlp.training.corpus_features import CorpusFeatures
@@ -10,17 +12,19 @@ class CorpusBrief:
         self.words_by_root = {}  # type: Dict[int, List[Tuple[str, str, str, str, int]]]
         self.words_total = 0
         self.word_grams = {}  # type: Dict[int, List[str, int]]
+        self.symbol_frequency = []  # type: List[str, float]
 
     def to_dict(self):
         return {
             'words_by_morph': self.words_by_morph,
             'words_by_root': self.words_by_root,
             'words_total': self.words_total,
-            'word_grams': self.word_grams
+            'word_grams': self.word_grams,
+            'symbol_frequency': self.symbol_frequency
         }
 
     def build_from_corpus(self, f: CorpusFeatures):
-        # build most popula
+        # build most popular
         max_len = 7
         top_count = 30
 
@@ -56,3 +60,31 @@ class CorpusBrief:
         for wg_len in self.word_grams:
             self.word_grams[wg_len].sort(key=lambda w: -w[1])
             self.word_grams[wg_len] = self.word_grams[wg_len][:30]
+
+        self.calculate_symbol_frequency(f)
+
+    def calculate_symbol_frequency(self, f: CorpusFeatures):
+        smb_count = {}  # type: Dict[str, int]
+        if os.path.isfile(f.corpus_path):
+            self.calculate_symbol_frequency_in_file(f.corpus_path, smb_count)
+        else:
+            files = [f for f in os.listdir(f.corpus_path)]
+            for file_name in files:
+                full_path = os.path.join(f.corpus_path, file_name)
+                if not os.path.isfile(full_path) or not file_name.endswith('.txt'):
+                    continue
+                self.calculate_symbol_frequency_in_file(full_path, smb_count)
+        total = sum([smb_count[s] for s in smb_count])
+        self.symbol_frequency = [(s, smb_count[s] / total,) for s in smb_count]
+        self.symbol_frequency.sort(key=lambda s: -s[1])
+
+    def calculate_symbol_frequency_in_file(self, file_path: str, smb_count: Dict[str, int]):
+        with codecs.open(file_path, 'r', encoding='utf-8') as f:
+            while True:
+                c = f.read(1)
+                if not c:
+                    break
+                if c == ' ' or c == '\n':
+                    continue
+                ct = smb_count.get(c) or 0
+                smb_count[c] = ct + 1
